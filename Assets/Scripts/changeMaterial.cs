a53e00c6e72bb3620850a96e0ba6c1e5bc355143
nesherben <nestor8299@gmail.com>
2019-11-16 14:45:19 +0100

    botones cambian texturas

---
+++ Assets/Scripts/changeMaterial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class changeMaterial : MonoBehaviour
+{
+    //Set these Textures in the Inspector
+    public Texture m_MainTexture, m_Normal, m_Metal, m_emision;
+    Renderer m_Renterer;
+    public GameObject objeto;
+    
+
+    void Start() {
+        //Fetch the Renderer from the GameObject
+        m_Renterer = objeto.GetComponent<MeshRenderer>();
+
+        //Make sure to enable the Keywords
+   
+        
+
+    }
+    // Use this for initialization
+    public void setTexture()
+    {
+        
+        m_Renterer.material.EnableKeyword("_NORMALMAP");
+        m_Renterer.material.EnableKeyword("_METALLICGLOSSMAP");
+        m_Renterer.material.EnableKeyword("_EMISSION");
+        //Set the Texture you assign in the Inspector as the main texture (Or Albedo)
+        m_Renterer.material.SetTexture("_MainTex", m_MainTexture);
+        //Set the Normal map using the Texture you assign in the Inspector
+        m_Renterer.material.SetTexture("_BumpMap", m_Normal);
+        //Set the Metallic Texture as a Texture you assign in the Inspector
+        m_Renterer.material.SetTexture("_MetallicGlossMap", m_Metal);
+        //Set material emissive Texture
+        m_Renterer.material.SetTexture("_EmissionMap", m_emision);
+        
+    }
+}
