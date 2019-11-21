c23e2d9b09807f03710a4c9f2a17b3c0ffa9560a
nesherben <nestor8299@gmail.com>
2019-11-21 03:33:18 +0100

    bugfixing

--- Assets/Scripts/audioControl.cs
+++ Assets/Scripts/audioControl.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class audioControl : MonoBehaviour
 {
+    public Slider slider;
     public AudioMixer mixer;
+    public static float saveValue = 1;
     public void setvolume(float sliderValue)
     {
         mixer.SetFloat("musicVol",Mathf.Log10(sliderValue)*20);
+        saveValue = sliderValue;
+    }
+    private void Start()
+    {
+        slider.value = saveValue;
     }
 }
