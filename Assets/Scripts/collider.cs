38fe625671deb4e8e5048475f906931ead440373
nesherben <nestor8299@gmail.com>
2019-11-21 21:37:19 +0100

    no message

---
+++ Assets/Scripts/collider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("cubo choca");
+    }
+}
