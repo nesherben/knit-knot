43847535410de80a872cd5dec86abae9deea5bd4
nesherben <nestor8299@gmail.com>
2019-11-27 20:23:28 +0100

    no message

--- Assets/Scripts/muteAudio.cs
+++ Assets/Scripts/muteAudio.cs
@@ -8,52 +8,48 @@ public class muteAudio : MonoBehaviour
 {
     public Image cambio;
     public Slider slider;
-    public GameObject boton,sonido;
+    public GameObject boton, sonido;
     public Sprite activo, inactivo;
     AudioSource miaudio;
     public bool muted = false;
     public float volumen, saved;
 
 
-    
+
     private void Start()
     {
+
         saved = 1f;
         miaudio = sonido.GetComponent<AudioSource>();
         cambio = boton.GetComponent<Image>();
         miaudio.volume = saved;
-        
-        
+
+
     }
 
-    public void muteado() {
+    public void muteado()
+    {
 
         if (!muted)
         {
-            cambio.sprite = activo;
             slider.value = 0;
             miaudio.volume = slider.value;
             muted = true;
-            
+            cambio.sprite = activo;
+
         }
         else
         {
             cambio.sprite = inactivo;
-            slider.value = saved;
-            miaudio.volume = slider.value;
+            miaudio.volume = saved;
             muted = false;
-            
+            slider.value = miaudio.volume;
         }
-        //setvolume();
+
     }
     public void setvolume()
     {
         miaudio.volume = slider.value;
-        //saved = slider.value;
+        saved = slider.value;
     }
-    
-
-
 }
-             
-       
